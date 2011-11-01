7da83bc120304cd896b0c55488efaedc8d5d92ba
Kai Yuan <kent.yuan@Gmail.com>
2011-11-01 23:05:00 +0100

    add README.md generator

--- shell/shadow.sh
+++ shell/shadow.sh
@@ -6,4 +6,14 @@
 # kent.yuan at gmail dot com
 #######################################################################
 
-convert $1 \( +clone -background black -shadow 55x15+0+5 \) +swap -background none -layers merge  +repage new.png
+
+if [ $# == 0 ]; then 
+	cat <<EOF
+	$0 adds shadow to given png image. 
+	Usage: $0 <input png image>
+	output filename is original filename with "_new" suffix
+EOF
+    exit 2
+fi
+
+convert $1 \( +clone -background black -shadow 55x15+0+5 \) +swap -background none -layers merge  +repage $1_new.png
