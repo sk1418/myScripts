7b7e3ea1eae337e1bddc31b1b1740336c65d4392
Kai Yuan <kent.yuan@Gmail.com>
2014-04-06 02:21:11 +0200

    add 2py2.sh, set py2 as default py

---
+++ shell/2py2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+########################################
+# set python2 as default python 
+########################################
+sudo ln -sf /usr/bin/python2 /usr/bin/python
+sudo ln -sf /usr/bin/python2-config /usr/bin/python-config
+sudo ln -sf /usr/bin/pydoc2 /usr/bin/pydoc
