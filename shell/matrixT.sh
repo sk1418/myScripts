c05eb0ad8bc6f62d9a19e348a049c2b9111151f9
Kent@LsyHP <kent.yuan@Gmail.com>
2014-08-01 13:59:19 +0200

    [matrixT.sh] improvement

--- shell/matrixT.sh
+++ shell/matrixT.sh
@@ -8,17 +8,12 @@
 # 2011-07-07
 #
 #########################################
-fs=
+fs=${2:-" "}
 file=$1
 
-if [ $# == 2 ]; then
-	fs=$2
-fi
-
-awk  "BEGIN{FS='$fs'}{for (i=1;i<=NF;i++) a[i,NR]=$i; }END{
+awk  -v FS="$fs" '{for (i=1;i<=NF;i++) a[i,NR]=$i; }END{
     for(i=1;i<=NF;i++) {
         for(j=1;j<=NR;j++)
-            printf a[i,j]' ';
-        print ''
+			printf "%s%s", a[i,j], (j==NR? ORS:OFS);
     } 
-}" $file
+}' "$file"
