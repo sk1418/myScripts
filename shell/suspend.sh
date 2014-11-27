8b73d3b57109fd11bf5205277eee016a4a74784d
Kent@LsyHP <kent.yuan@Gmail.com>
2014-11-27 14:08:15 +0100

    add y/n input to suspend

--- shell/suspend.sh
+++ shell/suspend.sh
@@ -5,8 +5,14 @@
 USB_DIR="/sys/bus/pci/drivers/xhci_hcd"
 IDS=$(\ls -1 $USB_DIR|grep ':')
 echo "rebinding all usb ports ..."
-echo -n "$IDS" > "$USB_DIR/unbind" && sudo echo -n "$IDS"> "$USB_DIR/bind"
-echo "suspending system ..."
-systemctl suspend
+sudo echo -n "$IDS" > "$USB_DIR/unbind" && sudo echo -n "$IDS"> "$USB_DIR/bind" 
+echo "Done."
 
- 
+while true; do
+    read -p "Suspend system?" yn
+    case $yn in
+        [Yy]* ) systemctl suspend; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
