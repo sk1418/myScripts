47fde5f69af83188c666bd03b382e9d7d7c936e7
Kent@LsyHP <kent.yuan@Gmail.com>
2014-12-22 13:31:14 +0100

    suspend.sh output

--- shell/suspend.sh
+++ shell/suspend.sh
@@ -6,15 +6,14 @@ USB_DIR="/sys/bus/pci/drivers/xhci_hcd"
 IDS=$(\ls -1 $USB_DIR|grep ':')
 echo "Rebinding all usb ports ..."
 sudo echo -n "$IDS" > "$USB_DIR/unbind" && sudo echo -n "$IDS"> "$USB_DIR/bind" 
-if [[ $? == "0" ]];then 
-	systemctl suspend
-else
-	while true; do
-		read -p "rebind failed, forgot 'sudo'?, suspend system anyway [y/n]?" yn
-		case $yn in
-			[Yy]* ) systemctl suspend; break;;
-			[Nn]* ) exit;;
-			* ) echo "Please answer y or n.";;
-		esac
-	done
-fi
+test $? == "0" && echo -e "\nDone" || echo -e "\nFAILED!"
+echo "====================================="
+
+while true; do
+    read -p "Continue to suspend system [y/n]?" yn
+    case $yn in
+        [Yy]* ) systemctl suspend; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
