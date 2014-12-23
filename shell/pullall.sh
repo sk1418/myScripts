bbed56b9eeb9159a6d7e16ae88140f285f5f4d54
Kent@LsyHP <kent.yuan@Gmail.com>
2014-12-23 10:31:42 +0100

    add $HOME/lib into pullall

--- shell/pullall.sh
+++ shell/pullall.sh
@@ -6,12 +6,19 @@
 # 2014-05-12 22:50:42 
 #==============================================
 
-CODE_BASE="/home/kent/MyStuff/myCodes"
+CODE_BASE=(/home/kent/MyStuff/myCodes 
+/home/kent/lib
+)
 EXCLUDE_PATT="gitTest"
 
-for line in $(find "$CODE_BASE" -name ".git"|grep -v "$EXCLUDE_PATT"); do
-	line=$(sed 's#/\.git##'<<<"$line")
-	repo=$(awk -F'/' '$0=$NF' <<<"$line")
-	echo "====> Pulling Repository: $repo <===="
-	git -C "$line" pull
+for base in ${CODE_BASE[@]};do
+	echo "##########################"
+	echo "   scanning $base"
+	echo "##########################"
+	for line in $(find "$base" -name ".git"|grep -v "$EXCLUDE_PATT"); do
+		line=$(sed 's#/\.git##'<<<"$line")
+		repo=$(awk -F'/' '$0=$NF' <<<"$line")
+		echo "====> Pulling Repository: $repo <===="
+		git -C "$line" pull
+	done
 done
