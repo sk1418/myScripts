1cad8c2a5668ecbcd1e50f4e20fc6ccbbfb81f64
Kai Yuan <kent.yuan@Gmail.com>
2015-12-05 22:26:59 +0100

    ffproxy add locate current pwd

--- shell/ffproxy.sh
+++ shell/ffproxy.sh
@@ -10,8 +10,16 @@
 #firefox dir
 PAC_FILE="$HOME/firefoxProxy.pac"
 
+script_path="${BASH_SOURCE[0]}"
+if ([ -L "${script_path}" ]) then
+	while ([ -L "${script_path}" ]) do script_path=$(readlink "${script_path}"); done
+fi
+pushd . > /dev/null
+cd $(dirname ${script_path}) > /dev/null
+script_path=$(pwd)
+
 echo "Loading the fastest cn proxy..."
-PROXY_RAW=$(python2 ../python/cnProxy.py|head -1)
+PROXY_RAW=$(python2 $script_path/../python/cnProxy.py|head -1)
 echo "$PROXY_RAW"
 PROXY=$(grep -oP '[0-9.]+:\d+' <<< "$PROXY_RAW")
 echo "==============================="
