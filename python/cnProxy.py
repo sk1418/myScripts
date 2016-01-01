02991714631cc8a1d6b578ebed8d85f732538421
Kai Yuan <kent.yuan@Gmail.com>
2016-01-01 16:36:10 +0100

    cnProxy, add peuland proxies

--- python/cnProxy.py
+++ python/cnProxy.py
@@ -9,6 +9,7 @@
 # Email  : kent dot yuan at gmail dot com.
 # 
 # update 2015-12-14  proxy-list.org has ip:port base64 encrypted....
+# update 2016-01-01  add peuland.com proxies
 ###################################################
 # -*- coding: utf-8 -*-
 
@@ -16,12 +17,16 @@ import sys, re, requests
 from bs4 import BeautifulSoup
 import base64
 
-PROXY_POOL_URL = 'http://proxy-list.org/english/search.php?search=CN&country=CN&type=any&port=any&ssl=any&p=%d'
+BORDER='>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+PROXY_POOL_1 = 'proxy-list.org proxies:'
+PROXY_POOL_URL_1 = 'http://proxy-list.org/english/search.php?search=CN&country=CN&type=any&port=any&ssl=any&p=%d'
+PROXY_POOL_2 = 'peuland.com proxies:'
+PROXY_POOL_URL_2 = 'https://proxy.peuland.com/proxy/search_proxy.php'
 
 class Proxy(object):
     def __init__(self, value, speed):
         self.value = value
-        self.speed = speed
+        self.speed = int(speed)
 
     def print_proxy(self):
         clear = u'\x1b[0m'
@@ -32,12 +37,12 @@ class Proxy(object):
         print '%s%s\t%s%skb%s' % (style["green"],self.value, style["red"], self.speed, clear)
 
 
-def parse_proxies():
+def parse_proxies_1():
     page = 1
     #empty list
     proxies = []
     while True:
-        html = requests.get(PROXY_POOL_URL%page).text
+        html = requests.get(PROXY_POOL_URL_1%page).text
         table = BeautifulSoup(html, 'html.parser').find(id="proxy-table")
         proxy_tags = table.find_all('script', text=re.compile(r'^\s*Proxy\s*\('))
         speed_tags = table.find_all('li',class_='speed', text=re.compile((r'[0-9.]k.*|-')))
@@ -53,10 +58,38 @@ def parse_proxies():
     if proxies:
         return sorted(proxies,key=lambda x : x.speed, reverse=True)
 
+def parse_proxies_2():
+    AGENT= 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
+    #headers
+    HEADERS = {'User-Agent':AGENT}
+    HEADERS['Referer'] = 'https://proxy.peuland.com/proxy_list_by_category.htm'
+    HEADERS['Cookie'] = 'peuland_id=35fefe23fedc52da9283ac5ed131cbab;peuland_md5=ca1f57155f5638ade3c28a900fbdbd55; w_h=1024; w_w=1280; w_cd=24; w_a_h=1024; w_a_w=1280'
+    proxies = []
+    s = requests.Session()
+    r= s.get("https://proxy.peuland.com/proxy_list_by_category.htm")
+    # print r.text
+    i=max_page = 1
+    while (i<=max_page):
+        r = s.post(PROXY_POOL_URL_2, headers=HEADERS, data = {"country_code":"cn", "search_type":"all","page":str(i)})
+        servers_json = r.json()['data']
+        for server in servers_json:
+            rate = base64.b64decode(server['time_downloadspeed'])
+            if rate <=7 :
+                continue
+            proxy = Proxy(\
+                        "%s:%s"%(base64.b64decode(server['ip']), \
+                                base64.b64decode(server['port'])),\
+                                rate)
+            proxies.append(proxy) 
+        max_page = int(r.json()['pagination']['maxpage'])
+        i+=1
+    if proxies:
+        return sorted(proxies,key=lambda x : x.speed, reverse=True)
 
 if __name__ == '__main__':
     n = 5 if len(sys.argv)<2 or int(sys.argv[1])<=0 else int(sys.argv[1])
-    proxies = parse_proxies()
+    #test peuland proxies only
+    proxies = parse_proxies_2()
     n = len(proxies) if len(proxies) < n else n
     for p in proxies[:n]:
         p.print_proxy()
